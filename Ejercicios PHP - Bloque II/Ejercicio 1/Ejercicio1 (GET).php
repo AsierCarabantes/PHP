006c2ea611b18acad39183acd04d7bf16aee0166
asier <acarabantesqu22dw@ikzubirimanteo.com>
2024-09-16 21:49:32 +0200

    Ejercicios PHP - Bloque II (Ejercicio 1)

---
+++ Ejercicios PHP - Bloque II/Ejercicio 1/Ejercicio1 (GET).php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <?php
+        $numero1 = $_GET['num1']; 
+        $numero2 = $_GET['num2'];
+
+        $suma = $numero1 + $numero2;
+        echo $numero1 . " + " . $numero2 . " = " . $suma; 
+        ?>
+    </body>
+</html>
