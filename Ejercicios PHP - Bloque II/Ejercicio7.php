290ca0494a86a6109b28221dfe21d12360808fb0
AsierCarabantes <acarabantesqu22dw@ikzubirimanteo.com>
2024-09-23 12:46:35 +0200

    Ejercicios PHP - Bloque II/OOP (Ejercicio 3)

--- Ejercicios PHP - Bloque II/Ejercicio7.php
+++ Ejercicios PHP - Bloque II/Ejercicio7.php
@@ -1,44 +1,73 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <style>
+            .error {color: red}
+        </style>
         <title>Ejercicio 7</title>
     </head>
     <body>
-        <h1>Formulario de registro</h1><br>
-        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-            Nombre: <input type="text" name="name"><br>
-            Correo electrónico: <input type="email" name="mail"><br>
-            Contraseña: <input type="password" name="pass1"><br>
-            Confirmar contraseña: <input type="password" name="pass2"><br><br>
-            <input type="submit">
-        </form>
-
         <?php
-            $nombre = $correo = $contraseña = $confirmarContraseña = "";
-            $nombreErr = $correoErr = $contraseñaErr = $confirmarContraseñaErr = "";
+            $nombre = $correo = $password = $confirmPassword = "";
+            $nombreErr = $correoErr = $passwordErr = $confirmPasswordErr = "";
 
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 if (empty($_POST["name"])) {
                     $nombreErr = "Campo obligatorio";
+                } else {
+                    $nombre = test_input($_POST["name"]);
+                    if (!preg_match("/^[A-Za-z\s]+$/", $nombre)) {
+                        $nombreErr = "Sólo letras mayúsculas, minúsculas y espacios";
+                    } 
                 }
 
                 if (empty($_POST["mail"])) {
-                    $nombreErr = "Campo obligatorio";
+                    $correoErr = "Campo obligatorio";
+                } else {
+                    $correo = test_input($_POST["mail"]);
+                    if (!preg_match("/^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/", $correo)) {
+                        $correoErr = "Formato no válido";
+                    }
                 }
 
                 if (empty($_POST["pass1"])) {
-                    $nombreErr = "Campo obligatorio";
+                    $passwordErr = "Campo obligatorio";
+                } else {
+                    $password = test_input($_POST["pass1"]);
+                    if (!preg_match("/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{6,}$/", $password)) {
+                        $passwordErr = "La contraseña debe tener al menos 6 caracteres, incluir una letra mayúscula, una minúscula, un número y un símbolo.";
+                    }
                 }
 
                 if (empty($_POST["pass2"])) {
-                    $nombreErr = "Campo obligatorio";
+                    $confirmPasswordErr = "Campo obligatorio";
+                } else {
+                    $confirmPassword = test_input($_POST["pass2"]);
+                    if ($confirmPassword != $password) {
+                        $confirmPasswordErr = "La contraseña no coincide";
+                    }
                 }
             }
 
             function test_input($data) {
-
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
             }
-        ?>
-        
+        ?>    
+
+            <h1>Formulario de registro</h1><br>
+            <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                Nombre: <input type="text" name="name">
+                <span class="error">* <?php echo $nombreErr;?></span><br>
+                Correo electrónico: <input type="email" name="mail">
+                <span class="error">* <?php echo $correoErr;?></span><br>
+                Contraseña: <input type="password" name="pass1">
+                <span class="error">* <?php echo $passwordErr;?></span><br>
+                Confirmar contraseña: <input type="password" name="pass2">
+                <span class="error">* <?php echo $confirmPasswordErr;?></span><br><br>
+            <input type="submit">
+        </form> 
     </body>
 </html>
---
+++ Ejercicios PHP - Bloque II/OPP/Ejercicio3.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 3</title>
+</head>
+<body>
+
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            Nombre de Usuario: <input type="text" name="nombre"><br>
+            Email: <input type="mail" name="email"><br>
+            Contraseña: <input type="password" name="contra"><br>
+            Repetir contraseña: <input type="password" name="repetirContra"><br>
+            <input type="submit"><br>
+    </form>
+
+    <?php
+        class Usuario {
+            private $nombre;
+            private $email;
+            private $password;
+            private $repetirPassword;
+
+            function __construct($nombre, $email, $password, $repetirPassword) {
+                $this->nombre = $nombre;
+                $this->email = $email;
+                $this->password = $password;
+                $this->repetirPassword = $repetirPassword;
+            }
+
+            function get_nombre () {
+                return $this->nombre;
+            }
+            function get_email () {
+                return $this->email;
+            }
+            function get_password () {
+                return $this->password;
+            }
+            function get_repetirPassword () {
+                return $this->repetirPassword;
+            }
+
+            function validatePassword() {
+                if ($this->get_password() == $this->get_repetirPassword()) {
+                    echo "Las contraseñas coinciden";
+                } else echo "Las contraseñas no coinciden";
+            }
+        }
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $nombre = $_POST["nombre"];
+                $correo = $_POST["email"];
+                $contraseña = $_POST["contra"];
+                $repetirContraseña = $_POST["repetirContra"];
+
+                $Usuario = new Usuario($nombre, $correo, $contraseña, $repetirContraseña);
+                $Usuario->validatePassword();
+            }
+
+    ?>
+</body>
+</html>
