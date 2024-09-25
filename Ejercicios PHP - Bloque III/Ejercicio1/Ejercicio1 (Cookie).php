097b4adcab28bfded186d21398208c0561357f03
AsierCarabantes <acarabantesqu22dw@ikzubirimanteo.com>
2024-09-25 09:39:54 +0200

    Ejercicios PHP - Bloque III (Ejercicio1)(Cookie)

--- Ejercicios PHP - Bloque III/Ejercicio1/Ejercicio1 (Cookie).php
+++ Ejercicios PHP - Bloque III/Ejercicio1/Ejercicio1 (Cookie).php
@@ -1,21 +1,21 @@
 <?php
     $cookie_nombre = "usuario";
     $cookie_valor = "Asier Carabantes";
-    setcookie($cookie_nombre, $cookie_valor, time() + (86400 * 365), "/");
+    setcookie($cookie_nombre, $cookie_valor, time() + (86400 * 365) , "/");
     
     if (isset($_COOKIE["visitas_" . $cookie_nombre])) {
         $visitas = $_COOKIE["visitas_" . $cookie_nombre] + 1;
     } else {
         $visitas = 1;
     }
-    setcookie("visitas_" . $cookie_nombre, 0, time() + (86400 * 365), "/");
 
     if (isset($_POST["reiniciar"])) {
-        setcookie("visitas_" . $cookie_nombre, 0, time() + (86400 * 365), "/");
         $visitas = 0;
-    } else {
-        setcookie("visitas_" . $cookie_nombre, $visitas, time() + (86400 * 365), "/");
+        header("Location: " . $_SERVER['PHP_SELF']);
     }
+
+    setcookie("visitas_" . $cookie_nombre, $visitas, time() + (86400 * 365), "/");
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -34,9 +34,10 @@
             echo "Cantidad de visitas: " . $visitas; 
         }
     ?>
+
     <br><br>
-    <form method="POST" action="">
-        <button type="submit" name="reiniciar">Reiniciar visitas</button>
+    <form method="POST">
+        <input type="submit" name="reiniciar">
     </form>
 </body>
 </html>
